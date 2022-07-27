864f9e536b43f503256cdb2c3c153fb41b8d9a71
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-07-27 14:12:42 -0500

    working code

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+// Import packages
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+// Import routes
+const routes = require('./routes');
+
+// Create a isProduction variable to check environment
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+// Initialize Express app
+const app = express();
+
+// Connect morgan, cookie-parser, .json middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+// Connect all routes
+app.use(routes);
+
+
+// Export app
+module.exports = app;
