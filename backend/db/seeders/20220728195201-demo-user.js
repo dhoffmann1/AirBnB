12987ab2d4bb74fa60b440ec1c78e48b74d05fc2
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-01 15:26:47 -0500

    Added firstName/lastName to User, created Booking model/mig, updated seeder info

--- backend/db/seeders/20220728195201-demo-user.js
+++ backend/db/seeders/20220728195201-demo-user.js
@@ -5,16 +5,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
+        firstName: 'Demo',
+        lastName: 'User',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Fake',
+        lastName: 'User1',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Fake',
+        lastName: 'User2',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
