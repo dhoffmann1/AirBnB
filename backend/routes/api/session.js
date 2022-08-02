8decd298383f370b17e52a49412e0714717834fe
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-01 23:49:54 -0500

    updated current session get

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -24,9 +24,7 @@ router.get(
   (req, res) => {
     const { user } = req;
     if (user) {
-      return res.json({
-        user: user.toSafeObject()
-      });
+      return res.json( user.toSafeObject());
     } else return res.json({});
   }
 );
