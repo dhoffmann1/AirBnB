84d892a612ad0910cb810457cee256356aea7e5f
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-02 11:31:37 -0500

    done with reviews seeder

--- backend/db/seeders/20220802155531-reviews.js
+++ backend/db/seeders/20220802155531-reviews.js
@@ -1,24 +1,39 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Reviews', [
+      {
+        review: 'greate house and host',
+        stars: 5,
+        userId: 2,
+        spotId:  1
+      },
+      {
+        review: 'your normal suburb house',
+        stars: 2,
+        userId:  3,
+        spotId:  1
+      },
+      {
+        review: 'convenient location in austin',
+        stars: 4,
+        userId:  1,
+        spotId:  2
+      },
+      {
+        review: 'great location near disney',
+        stars: 5,
+        userId:  1,
+        spotId:  3
+      },
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Reviews', {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {});
   }
 };
