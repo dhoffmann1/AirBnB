71f091d445bd2a195dd8b63754f74ef3fb9c1919
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-03 13:22:12 -0500

    created images endpoints

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const imagesRouter = require('./images.js');
 const { restoreUser, setTokenCookie, requireAuth } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
 
@@ -14,6 +15,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/images', imagesRouter);
 
 // // GET /api/restore-user
 // router.get(
