f8d9b9c5a478d4d2d9222b25ae759c4ebd2c781e
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-05 10:23:19 -0500

    changed model/mig data type

--- backend/db/migrations/20220801225738-create-booking.js
+++ backend/db/migrations/20220801225738-create-booking.js
@@ -21,11 +21,11 @@ module.exports = {
         onDelete: 'CASCADE'
       },
       startDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         allowNull: false
       },
       endDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         allowNull: false
       },
       createdAt: {
