0f54d5a383f1d7d060394269dfcbd7d85cc53c4d
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-04 19:20:39 -0500

    removed time from bookings seed data

--- backend/db/seeders/20220802155523-bookings.js
+++ backend/db/seeders/20220802155523-bookings.js
@@ -6,44 +6,44 @@ module.exports = {
       {
         spotId: 1,
         userId: 2,
-        startDate: '2022-02-12 07:00:00',
-        endDate:  '2022-02-16 15:00:00'
+        startDate: '2022-02-12',
+        endDate:  '2022-02-16'
       },
       {
         spotId: 1,
         userId: 3,
-        startDate:  '2022-02-18 07:00:00',
-        endDate:  '2022-02-22 15:00:00'
+        startDate:  '2022-02-18',
+        endDate:  '2022-02-22'
       },
       {
         spotId: 1,
         userId: 2,
-        startDate:  '2022-03-15 07:00:00',
-        endDate:  '2022-03-22 15:00:00'
+        startDate:  '2022-03-15',
+        endDate:  '2022-03-22'
       },
       {
         spotId: 2,
         userId: 1,
-        startDate:  '2022-05-12 07:00:00',
-        endDate:  '2022-05-15 15:00:00'
+        startDate:  '2022-05-12',
+        endDate:  '2022-05-15'
       },
       {
         spotId: 2,
         userId: 1,
-        startDate:  '2022-06-03 07:00:00',
-        endDate:  '2022-06-06 15:00:00'
+        startDate:  '2022-06-03',
+        endDate:  '2022-06-06'
       },
       {
         spotId: 3,
         userId: 1,
-        startDate:  '2022-06-15 07:00:00',
-        endDate:  '2022-06-30 15:00:00'
+        startDate:  '2022-06-15',
+        endDate:  '2022-06-30'
       },
       {
         spotId: 4,
         userId: 1,
-        startDate:  '2022-07-01 07:00:00',
-        endDate:  '2022-07-15 15:00:00'
+        startDate:  '2022-07-01',
+        endDate:  '2022-07-15'
       }
     ], {});
   },
