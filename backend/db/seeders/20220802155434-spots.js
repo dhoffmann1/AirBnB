666c19d052808416af140cf11068e87a7b867074
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-23 11:27:05 -0500

    updated seeders for more spots and images

--- backend/db/seeders/20220802155434-spots.js
+++ backend/db/seeders/20220802155434-spots.js
@@ -16,6 +16,42 @@ module.exports = {
         price: 125
       },
       {
+        ownerId: 1,
+        address: '247 Maverick St',
+        city: 'Kemah',
+        state: 'TX',
+        country: 'USA',
+        lat: 30.22,
+        lng: 97.60,
+        name: "Fully equipped 52' Houseboat",
+        description: '"Life is just a whisper in time." Feel like relaxing and enjoying life on the water with all the creature comforts of your home?',
+        price: 134
+      },
+      {
+        ownerId: 1,
+        address: '58 Leonard Ave',
+        city: 'Jonestown',
+        state: 'TX',
+        country: 'USA',
+        lat: 33.76,
+        lng: 105.37,
+        name: 'Hollows Beach Club',
+        description: 'Enjoy breathtaking views of Lake Travis while living the high life within The Hollows Resort Community.',
+        price: 545
+      },
+      {
+        ownerId: 1,
+        address: '1055 Hillcroft Ln',
+        city: 'Lakeway',
+        state: 'TX',
+        country: 'USA',
+        lat: 40.09,
+        lng: 101.12,
+        name: 'Tree House on Lake Travis #15',
+        description: 'Enjoy this peaceful beautiful lake view Treehouse on the south shore of Lake Travis. The "Treehouse on Lake Travis: Unit#15" offers 1st class amenities to give you a wonderful living space.',
+        price: 233
+      },
+      {
         ownerId: 2,
         address: '12 Washington Ave',
         city: 'Austin',
@@ -23,33 +59,93 @@ module.exports = {
         country: 'USA',
         lat: 30.27,
         lng: 97.74,
-        name: 'Demo-2-Apt',
-        description: '2/2 apt in complex with pool and grill',
-        price: 185
+        name: 'Work from Home in our Extremely Comfortable House',
+        description: 'Be prepared to be wowed by this brand-new space with modern farmhouse vibes. Walking through the sky-high front door, you will immediately notice the kitchen upgrades, vaulted ceilings, and comfortable gathering room.',
+        price: 103
+      },
+      {
+        ownerId: 2,
+        address: '16 San Jacinto Blvd',
+        city: 'Austin',
+        state: 'TX',
+        country: 'USA',
+        lat: 27.12,
+        lng: 99.57,
+        name: 'The Texas Suite by Rainey Street',
+        description: 'The Texas Suite is the perfect place to stay in downtown Austin if you are looking for convenient access to the Austin Convention Center and all of the bars, restaurants, and nightlife that the city has to offer.',
+        price: 190
+      },
+      {
+        ownerId: 2,
+        address: '66 Red River St',
+        city: 'Austin',
+        state: 'TX',
+        country: 'USA',
+        lat: 28.27,
+        lng: 92.74,
+        name: "The Marfa Suite in Downtown Austin's City Center",
+        description: 'Enjoy your stay in our studio condo with a Walk Score of 97! See the best of Austin with little effort as you will be in the very heart of all the action, just next door to the Austin Convention Center.',
+        price: 158
+      },
+      {
+        ownerId: 2,
+        address: '13 East 6th St',
+        city: 'Austin',
+        state: 'TX',
+        country: 'USA',
+        lat: 27.55,
+        lng: 98.11,
+        name: "Condo in the Heart of Downtown!",
+        description: 'Located right in the heart of downtown Austin. Walk to Rainey St, 6th St, Congress, Convention Center and the Captiol. No need to rent a car while staying here!',
+        price: 147
       },
       {
         ownerId: 3,
-        address: '247 Mickey Way',
+        address: '247 Mickey Way #22',
         city: 'Orlando',
         state: 'FL',
         country: 'USA',
         lat: 28.54,
-        lng: 81.38,
+        lng: 152.38,
         name: 'La Riviera Unit 22',
         description: 'Beautiful 4/2 home just 15 minutes away from Disney',
         price: 215
       },
       {
         ownerId: 3,
-        address: '247 Mickey Way',
+        address: '247 Mickey Way #23',
         city: 'Orlando',
         state: 'FL',
         country: 'USA',
         lat: 28.54,
-        lng: 81.38,
+        lng: 152.38,
         name: 'La Riviera Unit 23',
         description: 'Beautiful 3/2 home just 15 minutes away from Disney',
         price: 195
+      },
+      {
+        ownerId: 3,
+        address: '16 Vistana Resort Trl',
+        city: 'Orlando',
+        state: 'FL',
+        country: 'USA',
+        lat: 22.54,
+        lng: 151.38,
+        name: 'NOT A BORING HOUSE , THIS UNIT WITH ALL THE FUN !',
+        description: 'Make some memories at this unique and family-friendly place.  YOU MUST BE 21+ FOR THIS RESERVATION',
+        price: 69
+      },
+      {
+        ownerId: 3,
+        address: '77 Woodpiper Ln',
+        city: 'Orlando',
+        state: 'FL',
+        country: 'USA',
+        lat: 23.59,
+        lng: 154.07,
+        name: '1BR Villa Family Resort near Disney - Free Parking',
+        description: 'Worry free location. Short driving distance to Premium Outlets & Convention Center.',
+        price: 107
       },
     ], {});
   },
@@ -57,7 +153,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Spots', {
-      name: { [Op.in]: ['Demo-1-Home', 'Demo-2-Apt', 'La Riviera Unit 22', 'La Riviera Unit 23'] }
+      ownerId: { [Op.in]: [1, 2, 3] }
     }, {});
   }
 };
--- backend/db/seeders/20220802155536-images.js
+++ backend/db/seeders/20220802155536-images.js
@@ -4,18 +4,424 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Images', [
       {
-        url: 'URLaddress1',
-        previewImage: false,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-698416124763471250/original/5be19617-ead3-41c7-8c27-2e0895453daa.jpeg',
+        previewImage: true,
         spotId: 1,
-        reviewId:  null,
+        reviewId: null,
         userId: 1
       },
       {
-        url: 'URLaddress2',
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-698416124763471250/original/1e44799e-df14-4882-9fd7-77237d16adb2.jpeg',
         previewImage: false,
         spotId: 1,
-        reviewId:  null,
+        reviewId: null,
         userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-698416124763471250/original/bb096d63-cbdb-4bfd-b0cb-842e61ecb791.jpeg',
+        previewImage: false,
+        spotId: 1,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-698416124763471250/original/59f97b9f-a158-40bd-83f8-7b32735f66ac.jpeg',
+        previewImage: false,
+        spotId: 1,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-698416124763471250/original/bf178ceb-fbf8-414d-8cee-e68c243b2d06.jpeg',
+        previewImage: false,
+        spotId: 1,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/4b365e55-5925-492c-a3b1-eb77cba8082d.jpg',
+        previewImage: true,
+        spotId: 2,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/7718d584-c269-4e7d-a852-35d39aecd966.jpg',
+        previewImage: false,
+        spotId: 2,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/105485898/dabe6640_original.jpg',
+        previewImage: false,
+        spotId: 2,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/105486000/6b9d7190_original.jpg',
+        previewImage: false,
+        spotId: 2,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/105485860/a95c1f42_original.jpg',
+        previewImage: false,
+        spotId: 2,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/0f4191b6-871c-4217-ad97-762cfc3bce42.jpg',
+        previewImage: true,
+        spotId: 3,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/65f76c0c-7d2d-47ba-b18b-338c94912123.jpg',
+        previewImage: false,
+        spotId: 3,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/2ee7f56f-c3de-492b-a067-4278dbfb1bde.jpg',
+        previewImage: false,
+        spotId: 3,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/8cf3f9b0-b7cf-488f-9998-c3c6b06aef08.jpg',
+        previewImage: false,
+        spotId: 3,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/fe5a7fea-99bf-4c64-b0d3-3b4dae3ab53d.jpg',
+        previewImage: false,
+        spotId: 3,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-578991354244710147/original/b23338fa-0b35-4c53-b3c3-7c83d2ccba77.jpeg',
+        previewImage: true,
+        spotId: 4,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-578991354244710147/original/5cb24bc3-d3d4-4c82-8bbc-61647d24fa37.jpeg',
+        previewImage: false,
+        spotId: 4,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/e650f477-695c-44b6-a0f8-b65ec4da1870.jpg',
+        previewImage: false,
+        spotId: 4,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-578991354244710147/original/70c01f42-2fda-484f-b405-2cdd9f1879b6.jpeg',
+        previewImage: false,
+        spotId: 4,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-578991354244710147/original/92c6e46b-095f-4e2c-bda3-e7ca66575509.jpeg',
+        previewImage: false,
+        spotId: 4,
+        reviewId: null,
+        userId: 1
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-691966910150870405/original/8369f065-b294-4a1b-8177-9f4143c8cef8.jpeg',
+        previewImage: true,
+        spotId: 5,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-691966910150870405/original/59ad75f7-2140-4b5c-858f-5d38c39d15c2.jpeg',
+        previewImage: false,
+        spotId: 5,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-691966910150870405/original/549bb5fa-76c7-4887-8fb9-64470185ed03.jpeg',
+        previewImage: false,
+        spotId: 5,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-691966910150870405/original/d0ef82c0-a61c-43e4-ab55-079b62530577.jpeg',
+        previewImage: false,
+        spotId: 5,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-691966910150870405/original/1e2d5e1e-ffc3-4526-be6b-c80cc160134a.jpeg',
+        previewImage: false,
+        spotId: 5,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/9ce5e6a8-af80-4cd0-a1a2-419c6b7fcab8.jpg',
+        previewImage: true,
+        spotId: 6,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/16ad070b-eb87-46b9-b7ec-3a4684fe6383.jpg',
+        previewImage: false,
+        spotId: 6,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/a86b4867-e50b-4c39-bb0d-20c8e0781480.jpg',
+        previewImage: false,
+        spotId: 6,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/75fff438-53cb-4c3a-959d-88c88aab55ac.jpg',
+        previewImage: false,
+        spotId: 6,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/e2e608f3-469a-4714-b8ac-d5926eabc8df.jpg',
+        previewImage: false,
+        spotId: 6,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/a988003e-d6df-4eff-9038-7222456856d7.jpg',
+        previewImage: true,
+        spotId: 7,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/d6c0cdd7-6245-48af-bc2a-66eb76b46aee.jpg',
+        previewImage: false,
+        spotId: 7,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/f3df1506-acf2-4d34-a451-7dcb9dfe858b.jpg',
+        previewImage: false,
+        spotId: 7,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/cd5b8f84-d50d-446f-806d-0c17e46a03e9.jpg',
+        previewImage: false,
+        spotId: 7,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/0e9ac25d-6b99-413d-b1d7-0a39d0465764.jpg',
+        previewImage: false,
+        spotId: 7,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48850515/original/592773a1-3461-4799-a225-843aeb3c9e3c.jpeg',
+        previewImage: true,
+        spotId: 8,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48850515/original/d5144b3e-f49c-43a8-bd29-6ad61b5abd69.jpeg',
+        previewImage: false,
+        spotId: 8,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/2fc9bc5e-ae72-4edb-9d59-7f587669d541.jpg',
+        previewImage: false,
+        spotId: 8,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-48850515/original/e462af9c-a87e-4be4-b3c4-f71bd5d35522.png',
+        previewImage: false,
+        spotId: 8,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/b27f7a6b-ac8b-438a-91b0-bbcb52fe2211.jpg',
+        previewImage: false,
+        spotId: 8,
+        reviewId: null,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/cc6ae5e1-2b46-4b8e-9018-93ec5e912518.jpg',
+        previewImage: true,
+        spotId: 9,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/0b6efb5f-ff13-469a-bc36-fa3784fdf409.jpg',
+        previewImage: false,
+        spotId: 9,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/7a0dadc9-f285-4c6b-9a9c-6980f9c94be6.jpg',
+        previewImage: false,
+        spotId: 9,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/a0dbe3e4-a9aa-4625-83cd-c10f76b332d3.jpg',
+        previewImage: false,
+        spotId: 9,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/bc670a9f-4835-4b0d-b730-7e620da745d3.jpg',
+        previewImage: false,
+        spotId: 9,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/cc71c5c3-cd76-495f-bf85-a0d358146af7.jpg',
+        previewImage: true,
+        spotId: 10,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/a00035ce-95c0-47b1-9079-c45564889400.jpg',
+        previewImage: false,
+        spotId: 10,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/62b5dfd7-74fa-4d1d-8544-ec7b1fdae1d2.jpg',
+        previewImage: false,
+        spotId: 10,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/8ee703b2-3110-4fa3-b4a0-7c16edc5c773.jpg',
+        previewImage: false,
+        spotId: 10,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/877c1893-e6ae-4b3f-9a66-bbced8de0377.jpg',
+        previewImage: false,
+        spotId: 10,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-696402115582168566/original/4d29a813-83d7-4334-b92e-241bb18df1d3.jpeg',
+        previewImage: true,
+        spotId: 11,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-696402115582168566/original/38dea2b8-d42d-494a-8f31-39ab38667b62.jpeg',
+        previewImage: false,
+        spotId: 11,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-696402115582168566/original/62484862-1ad6-4e25-a432-dc9875d25d20.jpeg',
+        previewImage: false,
+        spotId: 11,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-696402115582168566/original/0e472dad-b4a0-42c5-84eb-9e70db0643a5.jpeg',
+        previewImage: false,
+        spotId: 11,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-696402115582168566/original/74a2510a-0716-42cb-afc2-113d8c541278.jpeg',
+        previewImage: false,
+        spotId: 11,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-687109903429635161/original/a991d7ef-10b0-48c4-b1c3-a5264e258ef6.jpeg',
+        previewImage: true,
+        spotId: 12,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-695027034953309034/original/36268428-1ec3-437f-93ad-73f442848d47.jpeg',
+        previewImage: false,
+        spotId: 12,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-695027034953309034/original/b55738fd-aaac-46a9-b30d-fc2f9e875290.jpeg',
+        previewImage: false,
+        spotId: 12,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-687109903429635161/original/df18ecaf-4096-44d2-950a-71e22f54be4e.jpeg',
+        previewImage: false,
+        spotId: 12,
+        reviewId: null,
+        userId: 3
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-687109903429635161/original/54a5ab54-490f-4021-8feb-a82d0856498c.jpeg',
+        previewImage: false,
+        spotId: 12,
+        reviewId: null,
+        userId: 3
       },
       {
         url: 'URLaddress3',
@@ -25,54 +431,19 @@ module.exports = {
         userId: 2
       },
       {
-        url: 'URLaddress4',
-        previewImage: true,
-        spotId: 2,
-        reviewId:  null,
-        userId: 2
-      },
-      {
-        url: 'URLaddress5',
-        previewImage: false,
-        spotId: 2,
-        reviewId:  null,
-        userId: 2
-      },
-      {
-        url: 'URLaddress6',
-        previewImage: true,
-        spotId: 3,
-        reviewId:  null,
-        userId: 3
-      },
-      {
-        url: 'URLaddress7',
-        previewImage: false,
-        spotId: 3,
-        reviewId:  null,
-        userId: 3
-      },
-      {
-        url: 'URLaddress8',
-        previewImage: true,
-        spotId: 4,
-        reviewId:  null,
-        userId: 3
-      },
-      {
         url: 'URLaddress9',
         previewImage: true,
         spotId: null,
         reviewId: 3,
-        userId: 1
-      },
+        userId: 3
+      }
     ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Images', {
-      spotId: { [Op.in]: [1, 2, 3, 4] }
+      userId: { [Op.in]: [1, 2, 3] }
     }, {});
   }
 };
