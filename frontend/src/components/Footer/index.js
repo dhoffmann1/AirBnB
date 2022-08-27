c6b082b23c25f4a0551ec8bc676c96d438e168d1
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-27 04:06:26 -0500

    removed gdrive link

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer(){
       <div id='footer-80px-padding'>
           <div id='footer-copyright'><i class="fa-solid fa-copyright"></i>2022 Airbnb-Prime, Inc</div>
           <a id='footer-github-link' rel='noreferrer' href='https://github.com/Pepa90210/AirBnB' target={"_blank"}><i class="fa-brands fa-github"></i>Github</a>
-          <a id='footer-google-sheet-link' rel='noreferrer' href='https://docs.google.com/spreadsheets/d/1XIUaz5OaT1PRIY4zpx8zjr3gERrRbxx3GdcL_G9N9j0/edit#gid=1712141062' target={"_blank"}><i class="fa-brands fa-google-drive"></i>Google Drive</a>
+          {/* <a id='footer-google-sheet-link' rel='noreferrer' href='https://docs.google.com/spreadsheets/d/1XIUaz5OaT1PRIY4zpx8zjr3gERrRbxx3GdcL_G9N9j0/edit#gid=1712141062' target={"_blank"}><i class="fa-brands fa-google-drive"></i>Google Drive</a> */}
           <a id='footer-linkedin-link' rel='noreferrer' href='https://www.linkedin.com/in/danielhoffmann-1/' target={"_blank"}><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
       </div>
     </footer>
